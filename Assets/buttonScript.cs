22ac647ff4d913ded0b556fe8104a7015d220368
udpatil <udpatil@gmail.com>
2016-12-03 06:46:25 -0500

    got button onto the hololens

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonScript : MonoBehaviour {
+    public Button myButton;
+
+	// Use this for initialization
+	void Start () {
+        myButton = GetComponent<Button>();
+        myButton.onClick.AddListener(() => changeColor());
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void changeColor()
+    {
+        var colors = myButton.colors;
+        colors.normalColor = Color.red;
+        myButton.colors = colors;
+    }
+}
---
+++ Assets/testScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour {
+    public Light myLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("space"))
+        {
+            myLight.enabled = true;
+        } else
+        {
+            myLight.enabled = false;
+        }
+	}
+}
