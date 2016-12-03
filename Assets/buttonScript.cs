1ca0194c14094846606ca67c06755ab3d088b93c
udpatil <udpatil@gmail.com>
2016-12-03 09:49:01 -0500

    uday: contains keyboard entry into text boxes. shukan: contains alert message and testing for two location inputs and button onClickListener

--- Assets/buttonScript.cs
+++ Assets/buttonScript.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class buttonScript : MonoBehaviour {
+    public InputField input;
+    public InputField input2;
     public Button myButton;
+    public Text alert;
+    string val;
+    string val2;
 
 	// Use this for initialization
 	void Start () {
+        Debug.Log("Test");
+        input.text = "";
+        input2.text = "";
         myButton = GetComponent<Button>();
-        myButton.onClick.AddListener(() => changeColor());
-	}
+        try
+        {
+            myButton.onClick.AddListener(() => changeColor());
+        }
+        catch (System.Exception e)
+        {
+            //DO nothing
+        }
+    }
 
 
 	
@@ -20,8 +35,19 @@ public class buttonScript : MonoBehaviour {
 
     void changeColor()
     {
-        var colors = myButton.colors;
-        colors.normalColor = Color.red;
-        myButton.colors = colors;
+        
+
+        if (input.text.Equals("") || input2.text.Equals(""))
+        {
+            alert.text = "Please input both locations"; 
+        }
+        else
+        {
+            val = input.text;
+            val2 = input2.text;
+            Debug.Log(val);
+            Debug.Log(val2);
+            alert.text = "Calculating route....";
+        }
     }
 }
---
+++ Assets/typeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
+
+public class typeScript : MonoBehaviour, IPointerClickHandler {
+    UnityEngine.TouchScreenKeyboard keyboard;
+    public static string keyboardText = "";
+    public Button myButton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+        if (TouchScreenKeyboard.visible == false && keyboard != null)
+        {
+            if (keyboard.done == true)
+            {
+                keyboardText = keyboard.text;
+                keyboard = null;
+            }
+        }
+    }
+}
